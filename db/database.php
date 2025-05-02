1adc3bc4bb173e37b5b67a34cf9810032c129d5e
Shaun_R <161319179+ShaunRodrigo@users.noreply.github.com>
2025-05-02 18:46:43 +0200

    ADD: DB

---
+++ db/database.php
@@ -0,0 +1,17 @@
+<?php
+$host = 'localhost'; 
+$dbname = 'databaselesson'; 
+$username = 'root'; 
+$password = ''; 
+
+try {
+    
+    $dbh = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    r
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
+?>
